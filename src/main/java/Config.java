61d1fac5f82e12a7bf8940794c318a6a9e5093f4
gudkovst <69453922+gudkovst@users.noreply.github.com>
2022-12-10 23:33:04 +0700

    Add files via upload

---
+++ src/main/java/Config.java
@@ -0,0 +1,12 @@
+package config;
+
+import java.awt.*;
+
+public class Config {
+    public static int WIDTH = 600;
+    public static int HEIGHT = 600;
+    public static Color color = Color.white;
+    public static String resourcesPath = "src\\main\\resources\\";
+    public static String marksFile = resourcesPath + "marks.csv";
+    public static String pictureFormat = ".jpeg";
+}
